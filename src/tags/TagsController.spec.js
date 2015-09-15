daf33c25bf441f2993ffefbc697d49182c83ec65
Olof Landahl <olof.landahl@hiq.se>
2015-09-15 10:23:04 +0200

    Added test

--- src/tags/TagsController.spec.js
+++ src/tags/TagsController.spec.js
@@ -34,9 +34,42 @@ describe('TagsController', function() {
       $httpBackend.verifyNoOutstandingRequest();
     });
 
+    it('Should reset the active index', function() {
+      expect($scope.activeIndex).toBe(-1);
+    });
+
     it('Should show the resulting images', function() {
       expect($scope.tags.length).toBe(2);
       expect($scope.tags).toEqual(TEST_RESPONSE.data);
+    });
+
+  });
+
+  describe('Selecting a tag', function() {
+
+    var TEST_TAG = 'angular',
+      TEST_INDEX = 2,
+      TEST_RESPONSE = {
+        data: [
+          {id: '123'}, 
+          {id: '456'},
+          {id: '789'}
+        ]
+      };
+
+    beforeEach(function() {
+      $httpBackend.expect('JSONP', INSTAGRAM_API.BASE_PATH+TEST_TAG+'/media/recent'+INSTAGRAM_API.PARAMS).respond(TEST_RESPONSE);
+      $scope.selectTag(TEST_TAG, TEST_INDEX);
+      $httpBackend.flush();
+    });
+
+    afterEach(function() {
+      $httpBackend.verifyNoOutstandingExpectation();
+      $httpBackend.verifyNoOutstandingRequest();
+    });
+
+    it('Should set the selected tag as active', function() {
+      expect($scope.activeIndex).toBe(TEST_INDEX);
     });
 
   });
